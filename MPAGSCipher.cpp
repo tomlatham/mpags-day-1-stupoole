c6f3bb5126e1411b199f52390a5ae22e2efafc6b
Stu Poole <stuart.f.poole@gmail.com>
2019-10-11 22:46:19 +0100

    Added blank cipher files

---
+++ MPAGSCipher.cpp
@@ -0,0 +1,5 @@
+//
+// Created by stu on 11/10/2019.
+//
+
+#include "MPAGSCipher.h"
---
+++ MPAGSCipher.h
@@ -0,0 +1,14 @@
+//
+// Created by stu on 11/10/2019.
+//
+
+#ifndef MPAGS_CIPHER_GIT_MPAGSCIPHER_H
+#define MPAGS_CIPHER_GIT_MPAGSCIPHER_H
+
+
+class MPAGSCipher {
+
+};
+
+
+#endif //MPAGS_CIPHER_GIT_MPAGSCIPHER_H
