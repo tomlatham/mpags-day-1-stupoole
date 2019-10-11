9c3ee3e4191caf5c4f3680e1ba93cf4426763bb0
Stu Poole <stuart.f.poole@gmail.com>
2019-10-12 00:31:18 +0100

    Defined functions for each of the exercises and defined print for all currently used data types. Did up to Ex 4 (shows that int / int = int but anything with a double becomes double)

--- MPAGSCipher.cpp
+++ MPAGSCipher.cpp
@@ -6,23 +6,49 @@
 #include <iostream>
 #include <string>
 
-void printString(){
-  const std::string message {"Hello world?"};
-  std::cout << message << "\n";
-}
+void print(double input) { std::cout << input << std::endl;}
 
-int main()
-{
-  //This prints hello world to console
-  std::cout << "Hello World \n";
+void print(int input) { std::cout << input << std::endl;}
 
+void print(char input) { std::cout << input << std::endl;}
+
+void print(const std::string& input) { std::cout << input << std::endl;}
+
+
+void exercise1(){  std::cout << "Hello World \n";}
+
+void exercise2(){
   int anInt{1};
   double aDouble{1.111};
   anInt++;
   aDouble++;
   std::cout << aDouble << std::endl;
   std::cout << anInt << std::endl;
-  printString();
+}
+
+void exercise3(){
+  const std::string message {"Hello world?"};
+  std::cout << message << "\n";
+}
+
+void exercise4(){
+  double d{6};
+  int i{4};
+  int j{5};
+  print(i*d);
+  print(d/i);
+  print(i/j);
+
+}
+
+int main()
+{
+  //This prints hello world to console
+
+  exercise1();
+  exercise2();
+  exercise3();
+  exercise4();
   return 0;
 }
 
