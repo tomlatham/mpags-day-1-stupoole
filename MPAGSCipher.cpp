7cbbbdf3be646ca94de6977d6bdb92ef4c4a433c
Stu Poole <stuart.f.poole@gmail.com>
2019-10-11 23:22:17 +0100

    variable declaration and varying in MPAGSCipher.cpp

--- MPAGSCipher.cpp
+++ MPAGSCipher.cpp
@@ -8,4 +8,12 @@ int main()
 {
 //    This prints hello world to console
     std::cout << "Hello World \n";
+
+    int an_int{1};
+    double a_double{1.111};
+    an_int ++;
+    a_double ++;
+    std::cout << a_double << std::endl;
+    std::cout << an_int << std::endl;
+
 }
